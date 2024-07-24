d523c76428381fc2c632604fe8c14b8950c45203
naha92 <148983183+naha92@users.noreply.github.com>
2024-07-24 14:56:52 +0100

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -3,19 +3,19 @@ sudo -i
 # update/upgrade system
 sudo apt-get update -y
 echo "apt updated"
-apt-get upgrade -y
+sudo apt-get upgrade -y
 echo "apt upgraded"
 # install nginx, MySQL, php
-apt-get install nginx -y; apt-get install mysql-server -y; apt-get install php-fpm php-mysql -y
+sudo apt-get install nginx -y; sudo apt-get install mysql-server -y; sudo apt-get install php-fpm php-mysql -y
 
 # start everything
-systemctl start nginx; systemctl start MySQL; systemctl start php7.4-fpm
+sudo systemctl start nginx; sudo systemctl start MySQL; sudo systemctl start php7.4-fpm
 
 # enable everything (will start after reboot)
-systemctl enable nginx; systemctl enable MySQL; systemctl enable php7.4-fpm
+sudo systemctl enable nginx; sudo systemctl enable MySQL; sudo systemctl enable php7.4-fpm
 
 # Secure MySQL installation (not done before, c+p chat)
-mysql_secure_installation <<EOF
+sudo mysql_secure_installation <<EOF
 
 y
 pa55word
@@ -48,9 +48,9 @@ echo "server {
 echo "<h1>Great Success!<h1>" > /var/www/html/index.html
 
 # nginx restart
-systemctl restart nginx
+sudo systemctl restart nginx
 
 # Create a test PHP file (not done before, c+p from chat)
-echo "<?php phpinfo(); ?>" | tee /var/www/html/info.php
+echo "<?php phpinfo(); ?>" | sudo tee /var/www/html/info.php
 
 echo "LEMP stack installation completed. You legend"
